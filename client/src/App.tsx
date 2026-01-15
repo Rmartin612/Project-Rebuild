841888bbb9469b925420eabc38b870cd52d951d8
RyanmartinCRE <ryanmartincre@gmail.com>
2026-01-14 20:46:22 -0700

    Add scroll to top on page navigation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route, Router as WouterRouter } from "wouter";
+import { Switch, Route, Router as WouterRouter, useLocation } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -8,12 +8,24 @@ import Home from "@/pages/Home";
 import About from "@/pages/About";
 import Volunteer from "@/pages/Volunteer";
 import Donate from "@/pages/Donate";
+import { useEffect } from "react";
 
 const base = import.meta.env.BASE_URL;
+
+function ScrollToTop() {
+  const [location] = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  return null;
+}
 
 function Router() {
   return (
     <WouterRouter base={base}>
+      <ScrollToTop />
       <Switch>
         <Route path="/" component={Home} />
         <Route path="/about" component={About} />
