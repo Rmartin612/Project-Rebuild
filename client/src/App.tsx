a2a4e5fb1785db53bc48deafe6871d69091f53e8
RyanmartinCRE <ryanmartincre@gmail.com>
2026-01-14 19:58:26 -0700

    Fix router base path for GitHub Pages deployment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router as WouterRouter } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -9,15 +9,19 @@ import About from "@/pages/About";
 import Volunteer from "@/pages/Volunteer";
 import Donate from "@/pages/Donate";
 
+const base = import.meta.env.BASE_URL;
+
 function Router() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/volunteer" component={Volunteer} />
-      <Route path="/donate" component={Donate} />
-      <Route component={NotFound} />
-    </Switch>
+    <WouterRouter base={base}>
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/volunteer" component={Volunteer} />
+        <Route path="/donate" component={Donate} />
+        <Route component={NotFound} />
+      </Switch>
+    </WouterRouter>
   );
 }
 
